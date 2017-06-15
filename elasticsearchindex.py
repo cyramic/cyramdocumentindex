ad596a48caa69bd1633e4ff8d2acee60c0979349
Matthew Pugh <pughmds@cyram.org>
2017-06-15 15:47:30 +0100

    Added comments

--- elasticsearchindex.py
+++ elasticsearchindex.py
@@ -1,5 +1,10 @@
+''' Simply indexes the docs in the docs directory in Elasticsearch
+ This would need to be a separate process that is run in the 
+ background eventually.  Ideally it would monitor the docs
+ directory and compare time stamps.  For now, this is pretty basic!'''
+
 import Words
 
-wobj = Words.Words()
-wobj.stopwords = Words.loadStopWords()
-Words.esIndexDocs(wobj.stopwords)
+if __name__ == "__main__":
+	wobj = Words.Words()
+	Words.esIndexDocs()
--- runapp.py
+++ runapp.py
@@ -1,25 +1,35 @@
+''' Web interface for the searching and display'''
+
 from flask import Flask, render_template
 import os, sys
 import json
 import Words
 
+# Load the words object to track data
 wobj = Words.Words()
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+	#Simple main index page
 	return render_template('index.html')
 	
 @app.route('/getwordlist/<num>/')
 def getWords(num):
+	# This renders the top {num} words in the datatable
 	topwords = wobj.getWords(int(num))
 	return render_template('wordlist.html', words=topwords)
 
 if __name__ == "__main__":
+	# Get list of words to ignore
 	wobj.stopwords = Words.loadStopWords()
+	
+	# Do my own word count frequency calculations
 	wobj.wordlist, wobj.wordfreq = Words.indexDocs(wobj.stopwords)
-
+	
+	# Sort the word count frequency dictionary from high to low
 	wobj.wordListToFreqDict()
 	wobj.sortFreqDict()
 
+	# Run the server
 	app.run()
